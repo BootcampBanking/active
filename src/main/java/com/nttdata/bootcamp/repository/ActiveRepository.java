36a5bc5a982df392357a2f277f4f8830db0b4a58
dmaldonf <dianacarolina8m@gmail.com>
2022-12-21 14:39:20 -0500

    update

--- src/main/java/com/nttdata/bootcamp/repository/ActiveRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nttdata.bootcamp.repository;
-
-import com.nttdata.bootcamp.entity.Active;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface ActiveRepository extends ReactiveCrudRepository<Active, String> {
-}
